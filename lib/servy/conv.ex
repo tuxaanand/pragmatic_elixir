9616087c874f4b34b12160e346270e08d8d3f0ea
Aanand Natarajan <aanand@gmail.com>
2020-12-06 21:03:48 +0530

    12. Modelling with Structs

---
+++ lib/servy/conv.ex
@@ -0,0 +1,23 @@
+
+defmodule Servy.Conv do
+
+	alias Servy.Conv
+
+	@http_codes %{
+			200 => "OK",
+			404 => "Not Found",
+			403 => "Forbidden",
+			405 => "Method Not Allowed",
+			500 => "Internal Server Error"
+	}
+
+	defstruct method: "", path: "", status: nil, resp_body: ""	
+
+	def full_status(%Conv{} = conv) do
+		"#{conv.status} #{status_text(conv.status)}"
+	end
+
+	defp status_text(code) do
+		@http_codes[code]
+	end
+end
